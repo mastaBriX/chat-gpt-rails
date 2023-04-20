134e31829ddd7c153d9b9637f750148ae3717af0
mastaBriX <xieyuze1996@yahoo.co.jp>
2023-04-20 17:13:41 +0900

    v0.5 使用mock api来模拟和gpt对话， 已实现实时对话的加载，布局美化。

--- config/initializers/redis_session_store.rb
+++ config/initializers/redis_session_store.rb
@@ -1,7 +1,7 @@
 # config/initializers/redis_session_store.rb
 
 Rails.application.config.session_store :redis_store,
-                                       servers: [ENV.fetch("SESSION_STORE_REDIS_URL")],
+                                       servers: [ENV.fetch("REDIS_URL_SESSION")],
                                        expire_after: 90.minutes,
                                        key: "_#{ENV.fetch("APP_NAME")}_session"
 
